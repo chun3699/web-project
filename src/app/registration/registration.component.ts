7a2568c167e2f1c6dd8495ea651c94ae9cdcff94
Saifa107 <162133763+Saifa107@users.noreply.github.com>
2024-03-18 21:31:09 +0700

    สร้าง regis

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
