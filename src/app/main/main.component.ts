f34d0cf41458b4ba9a7e9811237769ca280f2fbe
Saifa107 <162133763+Saifa107@users.noreply.github.com>
2024-03-18 23:28:05 +0700

    head หัว

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, 
+    MatDividerModule, 
+    MatIconModule,
+    MatToolbarModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
