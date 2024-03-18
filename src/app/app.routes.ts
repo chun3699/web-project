c1647741dd86adf5b9059aad2dea782e9842263e
Saifa107 <162133763+Saifa107@users.noreply.github.com>
2024-03-18 20:59:33 +0700

    import router-outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { MainComponent } from './main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login',component:LoginComponent},
+    {path:'regis',component:RegistrationComponent},
+    {path:'main',component:MainComponent}
+];
