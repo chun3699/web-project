c55b797dbfbf6d8ee3e272b43a3bd2275a629725
Saifa107 <162133763+Saifa107@users.noreply.github.com>
2024-03-18 21:38:50 +0700

    add vote

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { MainComponent } from './main/main.component';
+import { VoteComponent } from './vote/vote.component';
 
 export const routes: Routes = [
     {path:'login',component:LoginComponent},
     {path:'regis',component:RegistrationComponent},
-    {path:'main',component:MainComponent}
+    {path:'main',component:MainComponent},
+    {path:'vote',component:VoteComponent}
 ];
--- src/app/ranking/ranking.component.ts
+++ src/app/ranking/ranking.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-ranking',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './ranking.component.html',
   styleUrl: './ranking.component.scss'
 })
