9617b6702a614d2f54785a3ab8b0c1700ae606ca
Saifa107 <162133763+Saifa107@users.noreply.github.com>
2024-03-18 22:12:29 +0700

    add rank

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { MainComponent } from './main/main.component';
 import { VoteComponent } from './vote/vote.component';
+import { RankingComponent } from './ranking/ranking.component'; 
 
 export const routes: Routes = [
     {path:'login',component:LoginComponent},
     {path:'regis',component:RegistrationComponent},
     {path:'main',component:MainComponent},
-    {path:'vote',component:VoteComponent}
+    {path:'vote',component:VoteComponent},
+    {path:'rank',component:RankingComponent}
 ];
