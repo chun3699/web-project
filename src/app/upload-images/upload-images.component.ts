36c29a22180c93ef58dd72e4005dbaf14c78dd3c
Saifa107 <162133763+Saifa107@users.noreply.github.com>
2024-03-21 22:20:13 +0700

    ทดลองการupload

--- src/app/upload-images/upload-images.component.ts
+++ src/app/upload-images/upload-images.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component , OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-upload-images',
@@ -8,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrl: './upload-images.component.scss'
 })
 export class UploadImagesComponent {
+  constructor(private http: HttpClient){
+  }
+  ngOnInit():void{
 
+  }
+  onChangeFile(event : any ){
+    if(event.terget.files.length>0){
+      const file = event.traget.files[0];
+      if(file.type == 'image/png' || file.type == 'image/jpeg'){
+        const formData = new FormData();
+        formData.append('file',file);
+        this.http.post('http://localhost:3000/images/',formData).subscribe((res:any)=>{
+        debugger
+      });
+      }else {
+        alert ('Plase select only jpeg and png');
+      }
+      
+    }
+    
+  }
 }
