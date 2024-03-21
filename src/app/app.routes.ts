429a6a947536d6d4eb4eedf4eb475bc3d7b88ba7
Saifa107 <162133763+Saifa107@users.noreply.github.com>
2024-03-21 21:53:21 +0700

    add uploadimages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { RegistrationComponent } from './registration/registration.component';
 import { MainComponent } from './main/main.component';
 import { VoteComponent } from './vote/vote.component';
 import { RankingComponent } from './ranking/ranking.component'; 
+import { UploadImagesComponent } from './upload-images/upload-images.component';
 
 export const routes: Routes = [
     {path:'login',component:LoginComponent},
     {path:'regis',component:RegistrationComponent},
     {path:'main',component:MainComponent},
     {path:'vote',component:VoteComponent},
-    {path:'rank',component:RankingComponent}
+    {path:'rank',component:RankingComponent},
+    {path:'upload',component:UploadImagesComponent}
 ];
