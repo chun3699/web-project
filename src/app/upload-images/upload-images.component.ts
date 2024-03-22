b05c338534fcdf82a1c64003261fe1a94eb7d26d
Saifa107 <162133763+Saifa107@users.noreply.github.com>
2024-03-23 03:46:54 +0700

    upload delay

--- src/app/upload-images/upload-images.component.ts
+++ src/app/upload-images/upload-images.component.ts
@@ -18,8 +18,12 @@ const HOST: string = "http://localhost:3000";
 })
 export class UploadImagesComponent {
   imageUrl: string | null = null;
+  isFirstUpload: boolean = true;
   constructor(private http: HttpClient) {}
   ngOnInit():void{
+  }
+  async delay(ms: number) {
+    return await new Promise((resolve) => setTimeout(resolve, ms));
   }
   async onChangeFile(event: any) {
     if (event.target.files.length > 0) {
@@ -30,10 +34,21 @@ export class UploadImagesComponent {
         const url = `${HOST}/images/`;
         console.log(url)
         try {
+          // ตรวจสอบว่าเป็นการส่งข้อมูลครั้งแรกหรือไม่
+          if (!this.isFirstUpload) {
+            // ล่าช้าการส่งข้อมูลถัดไป 5 วินาที (5000 milliseconds)
+            alert('Data sent after 5 seconds delay');
+            await this.delay(5000);
+          }
           const response = await axios.post(url, formData);
           console.log('Success:', response.data); // Log the response data if needed
           // Set imageUrl to display the uploaded image
           this.imageUrl = response.data.imageUrl;
+
+          this.isFirstUpload = false;
+          // await this.delay(10000);
+          // console.log('10 seconds ' + new Date());
+        
         } catch (error) {
           console.error('Error:', error);
         }
