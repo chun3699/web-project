dc0fa22f0bc22af2cda009ddd8448cc356dc3a49
Saifa107 <162133763+Saifa107@users.noreply.github.com>
2024-03-25 02:28:07 +0700

    upload เสร็จ

--- src/app/services/api/service.service.ts
+++ src/app/services/api/service.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Constants } from '../../config/constants';
+
 import { lastValueFrom } from 'rxjs';
+import axios from "axios";
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +13,11 @@ export class ServiceService {
   constructor(private constants : Constants, private http: HttpClient) { }
 
   public async getUploadImega(body : any,options?: any){
-    const url = this.constants.API_ENDPOINT + '/images/uploadMySql';
+    const url = `${this.constants.API_ENDPOINT}/images/uploadMySql`;
     const response = await lastValueFrom(
-      this.http.post(url,JSON.stringify(body))
+      this.http.post(url,body)
     );
+    // const response = await axios.post(url,JSON.stringify(body));
     return response;
   }
 }
--- src/app/upload-images/upload-images.component.ts
+++ src/app/upload-images/upload-images.component.ts
@@ -48,22 +48,25 @@ export class UploadImagesComponent {
           //ออกมาเป็น array
           console.log(JSON.parse(JSON.stringify(response.data)));
 
-          // const imgArray = response.data.img; // ดึง array ของ URL จาก response.data
-          // console.log(imgArray);
-          // const imageUrl = imgArray[0];
+          const imgArray = response.data.img; // ดึง array ของ URL จาก response.data
+          console.log(imgArray);
+          const imageUrlString = imgArray[0];
+          console.log(imageUrlString);
 
            // Set imageUrl to display the uploaded image
           this.imageUrl = response.data.imageUrl;
           this.isFirstUpload = false;
 
-           //UploadImage in to Mysql
-          //  const body = {
-          //     img: imageUrl,
-          //     uid: 1
-          //  };
-          //  console.log(body);
-
-          //  const upImageMysql = await this.service.getUploadImega(body)
+          //  UploadImage in to Mysql
+           const body = {
+              img: imageUrlString,
+              uid: 1
+           };
+           console.log(body);
+           console.log(JSON.stringify(body));
+           const upImageMysql = await this.service.getUploadImega(body);
+           console.log(upImageMysql);
+           
         } catch (error) {
           console.error('Error:', error);
         }
