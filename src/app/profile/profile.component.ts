64cae89df0de80c41be9e195673939882d87330d
chun3699 <162800403+chun3699@users.noreply.github.com>
2024-03-28 22:09:17 +0700

    elo

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,20 +3,19 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ServiceService } from '../services/api/service.service';
 import { CommonModule } from '@angular/common';
 
-import { CardModule } from 'primeng/card';
-import { ButtonModule } from 'primeng/button';
+
+
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
-import { InputTextModule } from 'primeng/inputtext';
+
 import { User } from '../../model/model_uid';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CardModule,
+  imports: [
       CommonModule,
-      ButtonModule,
       MatButtonModule,
       MatCardModule,
       MatIconModule],
--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { ServiceService } from '../services/api/service.service';
 import { getImage } from '../../model/getImage';
+import { vote } from '../../model/vote';
 
 @Component({
   selector: 'app-vote',
@@ -20,6 +21,7 @@ export class VoteComponent {
   imgvote: getImage[] = []; //ภาพที่ชนะ[0] แพ้[1]
   images: getImage[] = []; 
   selectedImages: getImage[] = [];
+  score: vote[] = [];
 
   constructor(private service: ServiceService) {}
 
@@ -37,20 +39,49 @@ export class VoteComponent {
     this.selectedImages = [this.images[index1], this.images[index2]];
   }
 
-  calculateEloScore(scoreA: number, scoreB: number): number {
+  calculateEloScore_Win(scoreA: number, scoreB: number): void {
     const K = 32; // ค่าคงที่ K สำหรับ Elo Rating Algorithm
     const E = 1 / (1 + Math.pow(10, (scoreB - scoreA) / 400)); // คำนวณค่าคาดการณ์ความน่าจะเป็นของการชนะ
-    const S = 1; // ในกรณีนี้ สมมติว่าผู้ใช้โหวตภาพที่แสดงออกมาคือภาพที่ 1
+    console.log("win"+E);
+    const S = 1;
     let newScore = scoreA + K * (S - E); // คำนวณคะแนนใหม่
+    newScore = Math.round(newScore); // ทำเป็นจำนวนเต็ม
     if (newScore < 0) {
-        newScore = 0; // ตรวจสอบและกำหนดให้คะแนนใหม่ไม่ต่ำกว่า 0
+      newScore = 0; // ตรวจสอบและกำหนดให้คะแนนใหม่ไม่ต่ำกว่า 0
     }
-    return Math.round(newScore); // คืนค่าคะแนนใหม่เป็นจำนวนเต็ม
+      this.score[0].did = this.imgvote[0].did
+      this.score[0].score = newScore;
+      this.score[0].E = E;
+      this.score[0].K = K;
+      this.score[0].sA = scoreA;
+      this.score[0].sB = scoreB;
+      this.score[0].ss = K * (S - E);
+      console.log("win"+this.score[0].ss);
+  }
+
+  calculateEloScore_Lose(scoreA: number, scoreB: number): void {
+    const K = 32; // ค่าคงที่ K สำหรับ Elo Rating Algorithm
+    const E = 1 / (1 + Math.pow(10, (scoreB - scoreA) / 400)); // คำนวณค่าคาดการณ์ความน่าจะเป็นของการชนะ
+    console.log("Lose"+E);
+    const S = 0;
+    let newScore = scoreA + K * (S - E); // คำนวณคะแนนใหม่
+    newScore = Math.round(newScore); // ทำเป็นจำนวนเต็ม
+    if (newScore < 0) {
+      newScore = 0; // ตรวจสอบและกำหนดให้คะแนนใหม่ไม่ต่ำกว่า 0
+    }
+      this.score[1].did = this.imgvote[1].did
+      this.score[1].score = newScore;
+      this.score[1].E = E;
+      this.score[1].K = K;
+      this.score[1].sA = scoreA;
+      this.score[1].sB = scoreB;
+      this.score[1].ss = K * (S - E);
+      console.log("Lose"+this.score[0].ss);
   }
 
   vote(index: number): void {
     this.imgvote = [];
-
+    this
     if (this.selectedImages[0].did === index) {
       // ถ้ารูปภาพที่โหวตเป็นรูปภาพที่ 1
       this.imgvote.push(this.selectedImages[0]);
@@ -63,20 +94,15 @@ export class VoteComponent {
   }else{
     console.log("error");
   }
+  this.score[0] = {E: 0,sA: 0,sB: 0,K: 0,S: 0,score: 0,ss: 0,did: 0};
+  this.score[1] = {E: 0,sA: 0,sB: 0,K: 0,S: 0,score: 0,ss: 0,did: 0};
+  
+   this.calculateEloScore_Win(this.imgvote[0].vote_count, this.imgvote[1].vote_count);
+   this.calculateEloScore_Lose(this.imgvote[1].vote_count, this.imgvote[0].vote_count);
+   
+  
+  
 
-    
-  //   if (this.votedIndices.length >= 2) {
-  //       // คำนวณคะแนนโดยใช้ Elo Rating Algorithm
-  //       const image1Score = this.calculateEloScore(this.imageScores[0], this.imageScores[1]);
-  //       const image2Score = this.calculateEloScore(this.imageScores[1], this.imageScores[0]);
-
-  //       // แสดงผลลัพธ์บนหน้าจอ
-  //       console.log('คะแนนภาพที่ 1:', image1Score);
-  //       console.log('คะแนนภาพที่ 2:', image2Score);
-
-  //       // ล้างรายการโหวต
-       
-        
-  //   }
+  
   }
 }
