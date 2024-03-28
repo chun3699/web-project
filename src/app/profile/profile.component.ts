e91d24d76465a509930d10645f9a7774e489cd00
Saifa107 <162133763+Saifa107@users.noreply.github.com>
2024-03-28 19:21:36 +0700

    ส่งก่อนนะ

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import { InputTextModule } from 'primeng/inputtext';
+
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CardModule,
+      ButtonModule,
+      MatButtonModule,
+      MatCardModule,
+      MatIconModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
-
+  ProfileImage(){
+    console.log("ทำอยู่นะ");
+  }
 }
