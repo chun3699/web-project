20d62984de0f707f5866b497ce0c14c2f70c9fd8
Saifa107 <162133763+Saifa107@users.noreply.github.com>
2024-03-28 23:37:10 +0700

    add นะ

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,39 +1,100 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChange  } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ServiceService } from '../services/api/service.service';
-import { CommonModule } from '@angular/common';
+import axios from "axios";
+import { SimpleChanges } from '@angular/core';
+import { Input } from '@angular/core';
 
-import { CardModule } from 'primeng/card';
-import { ButtonModule } from 'primeng/button';
 import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
-import { InputTextModule } from 'primeng/inputtext';
+import { image } from '../../model/image';
+import { getImage } from '../../model/getImage';
 import { User } from '../../model/model_uid';
+
+const HOST: string = "http://localhost:3000";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CardModule,
+  imports: [
       CommonModule,
-      ButtonModule,
       MatButtonModule,
-      MatCardModule,
       MatIconModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
   showProfile: boolean = true;
+  imageUrl: string | null = null;
+  isFirstUpload: boolean = true;
   getUid : User [] = [];
+  
   constructor(private http: HttpClient,private service: ServiceService ){}
   async ngOnInit(){
     this.getUid = await this.service.UserIsYou();
+    this.showProfile;
     console.log(this.getUid);
     console.log(this.getUid[0].profile);
     console.log('Call Completed');
   }
-  ProfileImage(){
-    console.log("ทำอยู่นะ");
+  async ngOnChanges(changes : SimpleChanges){
+    this.getUid = await this.service.UserIsYou();
+    console.log("getUid");
+  }
+  async ProfileImage(event:any){
+      console.log("เจออะไรมั่ย");
+      if (event.target.files.length > 0) {
+        const file = event.target.files[0];
+        if (file.type == 'image/png' || file.type == 'image/jpeg') {
+          const formData = new FormData();
+          formData.append('image', file);
+          const url = `${HOST}/images/`;
+          console.log(url)
+          try {   
+            const response = await axios.post(url, formData);
+            console.log('Success:', response.data); // Log the response data if needed
+           
+            //ออกมาเป็น array
+            console.log(JSON.parse(JSON.stringify(response.data)));
+  
+            const imgArray = response.data.img; // ดึง array ของ URL จาก response.data
+            console.log(imgArray);
+            const imageUrlString = imgArray[0];
+            console.log(imageUrlString);
+             // Set imageUrl to display the uploaded image
+            this.imageUrl = response.data.imageUrl;
+  
+            //  UploadImage in to Mysql
+             const body = {
+              profile: imageUrlString, 
+             };
+             console.log(body);
+             console.log(JSON.stringify(body));
+             const UpProfile_P = await this.service.UpdateProfile(body,1);
+             console.log(UpProfile_P);
+             this.ngOnChanges({});
+             
+          } catch (error) {
+            console.error('Error:', error);
+          }
+      } else {
+        alert('Please select only jpeg and png');
+      }
+      }   
+  }
+
+  async btnClick(text: HTMLInputElement) {
+    if(text.value.length > 0){
+      console.log(text.value);
+      const body = {
+        name: text.value
+    };
+    const UpProfile_P = await this.service.UpdateProfile(body,1);
+    console.log(UpProfile_P);
+    this.ngOnChanges({});
+    text.value ='';
+    }
   }
 }
