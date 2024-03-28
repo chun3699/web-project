ed30a54e803daeb3623caeeb80d57baf9a726e7c
chun3699 <162800403+chun3699@users.noreply.github.com>
2024-03-28 23:44:59 +0700

    vote

--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -31,6 +31,9 @@ export class VoteComponent {
   }
 
   selectRandomImages() {
+    this.imgvote = [];
+    this.score = [];
+    this.selectedImages = [];
     const index1 = Math.floor(Math.random() * this.images.length);
     let index2 = Math.floor(Math.random() * this.images.length);
     while (index2 === index1) {
@@ -40,7 +43,7 @@ export class VoteComponent {
   }
 
   calculateEloScore_Win(scoreA: number, scoreB: number): void {
-    const K = 32; // ค่าคงที่ K สำหรับ Elo Rating Algorithm
+    const K = 10; // ค่าคงที่ K สำหรับ Elo Rating Algorithm
     const E = 1 / (1 + Math.pow(10, (scoreB - scoreA) / 400)); // คำนวณค่าคาดการณ์ความน่าจะเป็นของการชนะ
     console.log("win"+E);
     const S = 1;
@@ -52,6 +55,7 @@ export class VoteComponent {
       this.score[0].did = this.imgvote[0].did
       this.score[0].score = newScore;
       this.score[0].E = E;
+      this.score[1].S = S;
       this.score[0].K = K;
       this.score[0].sA = scoreA;
       this.score[0].sB = scoreB;
@@ -60,18 +64,21 @@ export class VoteComponent {
   }
 
   calculateEloScore_Lose(scoreA: number, scoreB: number): void {
-    const K = 32; // ค่าคงที่ K สำหรับ Elo Rating Algorithm
+    const K = 10; // ค่าคงที่ K สำหรับ Elo Rating Algorithm
     const E = 1 / (1 + Math.pow(10, (scoreB - scoreA) / 400)); // คำนวณค่าคาดการณ์ความน่าจะเป็นของการชนะ
     console.log("Lose"+E);
     const S = 0;
     let newScore = scoreA + K * (S - E); // คำนวณคะแนนใหม่
     newScore = Math.round(newScore); // ทำเป็นจำนวนเต็ม
+    console.log("news"+newScore);
+    
     if (newScore < 0) {
       newScore = 0; // ตรวจสอบและกำหนดให้คะแนนใหม่ไม่ต่ำกว่า 0
     }
       this.score[1].did = this.imgvote[1].did
       this.score[1].score = newScore;
       this.score[1].E = E;
+      this.score[1].S = S;
       this.score[1].K = K;
       this.score[1].sA = scoreA;
       this.score[1].sB = scoreB;
@@ -79,7 +86,7 @@ export class VoteComponent {
       console.log("Lose"+this.score[0].ss);
   }
 
-  vote(index: number): void {
+   vote(index: number): void {
     this.imgvote = [];
     this
     if (this.selectedImages[0].did === index) {
@@ -97,12 +104,29 @@ export class VoteComponent {
   this.score[0] = {E: 0,sA: 0,sB: 0,K: 0,S: 0,score: 0,ss: 0,did: 0};
   this.score[1] = {E: 0,sA: 0,sB: 0,K: 0,S: 0,score: 0,ss: 0,did: 0};
   
-   this.calculateEloScore_Win(this.imgvote[0].vote_count, this.imgvote[1].vote_count);
-   this.calculateEloScore_Lose(this.imgvote[1].vote_count, this.imgvote[0].vote_count);
-   
-  
-  
+  this.calculateEloScore_Win(this.imgvote[0].vote_count, this.imgvote[1].vote_count);
+  this.calculateEloScore_Lose(this.imgvote[1].vote_count, this.imgvote[0].vote_count);
 
-  
+  // เรียกใช้งาน putvote เพื่อส่งข้อมูลโหวตไปยัง API
+  this.putvote();
+
+  // เรียกใช้งาน delayRandomImages หลังจากที่คำนวณคะแนนแล้ว
+  this.delayRefresh();
+  }
+
+  delayRefresh(): void {
+    setTimeout(() => {
+      // รีเฟรชหน้าใหม่หลังจาก delay 3 วินาที
+      window.location.reload();
+    }, 1000);
+  }
+
+  async putvote(): Promise<void> {
+    for (let item of this.score) {
+      let did = item.did;
+      let score = item.score;
+      const vote = await this.service.vote(did, score);
+      console.log("vote" + vote);
+    }
   }
 }
