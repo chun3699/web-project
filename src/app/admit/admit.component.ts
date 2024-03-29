d0e714ae6ef866fd57ddc469763858c61f393fa2
Saifa107 <162133763+Saifa107@users.noreply.github.com>
2024-03-29 08:06:28 +0700

    add

--- src/app/admit/admit.component.ts
+++ src/app/admit/admit.component.ts
@@ -11,6 +11,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import { image } from '../../model/image';
 import { getImage } from '../../model/getImage';
+import { User } from '../../model/model_uid';
 
 
 @Component({
@@ -27,13 +28,13 @@ import { getImage } from '../../model/getImage';
   styleUrl: './admit.component.scss'
 })
 export class AdmitComponent {
-  getRank : getImage [] = []; 
+  admit_U : User [] = []; 
   constructor(private http: HttpClient,private service: ServiceService ) {
     
   }
   async ngOnInit(){
-    this.getRank = await this.service.appearRank();
-    console.log(this.getRank);
+    this.admit_U = await this.service.seleteAtUid();
+    console.log(this.admit_U);
     console.log('Call Completed');
     console.log('time ' + new Date());
   } 
